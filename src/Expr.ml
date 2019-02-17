c7090f88fc6384f8ccd6a8158d78065df9560594
Vladimir Turov <aaaaaa2493@yandex.ru>
2019-02-17 05:03:56 -0800

    HW1

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,34 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
+
+
+let to_bool (i: int): bool = i != 0
+let to_int (b: bool): int = if b then 1 else 0
+
+let calc (op: string) (left: int) (right: int): int = match op with
+  | "+" -> left + right
+  | "-" -> left - right
+  | "*" -> left * right
+  | "/" -> left / right
+  | "%" -> left mod right
+  | "<" -> to_int (left < right)
+  | ">" -> to_int (left > right)
+  | "<=" -> to_int (left <= right)
+  | ">=" -> to_int (left >= right)
+  | "==" -> to_int (left == right)
+  | "!=" -> to_int (left != right)
+  | "&&" -> to_int ( (to_bool left) && (to_bool right) )
+  | "!!" -> to_int ( (to_bool left) || (to_bool right) )
+
 
 (* Expression evaluator
 
@@ -50,5 +71,7 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval (st: state) (ex: expr): int = match ex with
+  | Const x -> x
+  | Var x -> st x
+  | Binop (op, left, right) -> calc op (eval st left) (eval st right) 
