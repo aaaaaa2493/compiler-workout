36d217960b76c24d75f32abe08dfe672548b0769
Vladimir Turov <aaaaaa2493@yandex.ru>
2019-05-03 06:35:59 -0700

    hw6

--- src/Language.ml
+++ src/Language.ml
@@ -57,7 +57,6 @@ module Expr =
     *)
       
     (* Expression evaluator
-
           val eval : state -> t -> int
  
        Takes a state and an expression, and returns the value of the expression in 
@@ -90,13 +89,12 @@ module Expr =
       | Binop (op, x, y) -> to_func op (eval st x) (eval st y)
 
     (* Expression parser. You can use the following terminals:
-
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
     ostap (                                      
-      parse:
+      expr:
 	  !(Ostap.Util.expr 
              (fun x -> x)
 	     (Array.map (fun (a, s) -> a, 
@@ -115,7 +113,7 @@ module Expr =
       primary:
         n:DECIMAL {Const n}
       | x:IDENT   {Var x}
-      | -"(" parse -")"
+      | -"(" expr -")"
     )
     
   end
@@ -140,21 +138,57 @@ module Stmt =
     type config = State.t * int list * int list 
 
     (* Statement evaluator
-
          val eval : env -> config -> t -> config
-
        Takes an environment, a configuration and a statement, and returns another configuration. The 
        environment supplies the following method
-
            method definition : string -> (string list, string list, t)
-
        which returns a list of formal parameters and a body for given definition
     *)
-    let rec eval _ = failwith "Not Implemented Yet"
+    let rec eval env ((state, stdin, stdout) as conf) stmt = match stmt with
+      | Read (x)         -> (
+        match stdin with
+          | i :: is -> (State.update x i state, is, stdout)
+          | _       -> failwith "Stdin is empty"
+      )
+      | Write (expr)     -> (state, stdin, stdout @ [Expr.eval state expr])
+      | Assign (x, expr) -> (State.update x (Expr.eval state expr) state, stdin, stdout)
+      | Seq (a, b)       -> eval env (eval env conf a) b
+      | Skip             -> conf
+      | If (expr, a, b)  -> eval env conf (if (Expr.eval state expr) != 0 then a else b)
+      | While (expr, a)  -> if (Expr.eval state expr) != 0 then eval env (eval env conf a) stmt else conf
+      | Repeat (a, expr) -> (
+        let (state', stdin', stdout') as conf' = eval env conf a in
+        if (Expr.eval state' expr) != 0 then conf' else eval env conf' stmt
+      )
+      | Call (f, args)  -> (
+        let (arg_names, local_names, f_stmt) = env#definition f in
+        let args = List.map (Expr.eval state) args in
+        let scope = State.push_scope state (arg_names @ local_names) in
+        let f_state = List.fold_left2 (fun state' x v -> State.update x v state') scope arg_names args in
+        let (state', stdin', stdout') = eval env (f_state, stdin, stdout) f_stmt in
+        (State.drop_scope state' state, stdin', stdout')
+      )
                                 
     (* Statement parser *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+    ostap (
+      stmt:
+        "read" "(" x:IDENT ")" {Read (x)} |
+        "write" "(" expr:!(Expr.expr) ")" {Write (expr)} |
+        "skip" {Skip} |
+        "if" expr:!(Expr.expr) "then" s1:parse s2:elsebranch {If (expr, s1, s2)} |
+        "while" expr:!(Expr.expr) "do" s1:parse "od" {While (expr, s1)} |
+        "for" s1:parse "," expr:!(Expr.expr) "," s2:parse "do" s3:parse "od" {Seq (s1, While (expr, Seq (s3, s2)))} |
+        "repeat" s1:parse "until" expr:!(Expr.expr) {Repeat (s1, expr)} |
+        f:IDENT "(" args:(!(Expr.expr))* ")" { Call (f, args) } |
+
+        x:IDENT ":=" expr:!(Expr.expr) {Assign (x, expr)};
+
+      elsebranch:
+        "fi" {Skip} |
+        "else" s1:parse "fi" {s1} |
+        "elif" expr:!(Expr.expr) "then" s1:parse s2:elsebranch {If(expr, s1, s2)};
+
+      parse: a:stmt ";" b:parse {Seq (a, b)} | stmt
     )
       
   end
@@ -166,8 +200,15 @@ module Definition =
     (* The type for a definition: name, argument list, local variables, body *)
     type t = string * (string list * string list * Stmt.t)
 
+    let default v opt = match opt with
+      | Some x -> x
+      | None   -> v
+
     ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+      parse:
+        "fun" f:IDENT "(" arg_names:(IDENT)* ")" local_names:(-"local" (IDENT)*)? "{"
+          stmt:!(Stmt.parse)
+        "}" { (f, (arg_names, default [] local_names, stmt)) }
     )
 
   end
@@ -178,9 +219,7 @@ module Definition =
 type t = Definition.t list * Stmt.t    
 
 (* Top-level evaluator
-
      eval : t -> int list -> int list
-
    Takes a program and its input stream, and returns the output stream
 *)
 let eval (defs, body) i =
--- src/SM.ml
+++ src/SM.ml
@@ -31,7 +31,76 @@ type config = (prg * State.t) list * int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let rec eval _ = failwith "Not Implemented Yet"
+let eval_insn (cs, stack, conf) insn =
+  let (state, stdin, stdout) = conf in match insn with
+  | BINOP (op) -> (
+      match stack with
+          | x :: y :: ss -> (cs, (Expr.to_func op y x) :: ss, conf)
+          | _            -> failwith "There is no enough element on the stack"
+  )
+  | CONST (n)  -> (cs, n :: stack, conf)
+  | READ       -> (
+      match stdin with
+          | i :: is -> (cs, i :: stack, (state, is, stdout))
+          | _       -> failwith "Stdin is empty"
+  )
+  | WRITE      -> (
+      match stack with
+          | x :: ss -> (cs, ss, (state, stdin, stdout @ [x]))
+          | _       -> failwith "Stack is empty"
+  )
+  | LD (x)     -> (cs, (State.eval state x) :: stack, conf)
+  | ST (x)     -> (
+      match stack with
+          | v :: ss -> (cs, ss, (State.update x v state, stdin, stdout))
+          | _       -> failwith "Stack is empty"
+  )
+  | _          -> failwith "Unsupported instruction"
+
+let rec eval env ((cs, stack, cnf) as conf) prog =
+    match prog with
+    | (LABEL _) :: ps     -> eval env conf ps
+    | (JMP l) :: _        -> eval env conf (env#labeled l)
+    | (CJMP (f, l)) :: ps -> (
+        match stack with
+            | x :: ss -> (
+                if f = "z" && x == 0 || f = "nz" && x != 0 then
+                    eval env (cs, ss, cnf) (env#labeled l)
+                else
+                    eval env (cs, ss, cnf) ps
+            )
+            | _ -> failwith "Stack is empty"
+    )
+    | (CALL l) :: ps      -> (
+        let (state, _, _) = cnf in
+        eval env ((ps, state) :: cs, stack, cnf) (env#labeled l)
+    )
+    | (BEGIN (arg_names, local_names)) :: ps -> (
+        let (state, stdin, stdout) = cnf in
+        let scope = State.push_scope state (arg_names @ local_names) in
+        let args = List.map (fun x -> ST x) arg_names in
+        eval env (cs, stack, (scope, stdin, stdout)) (args @ ps)
+    )
+    | (END) :: _ -> (
+        match cs with
+            | (ps, state') :: cs' -> (
+                let (state, stdin, stdout) = cnf in
+                eval env (cs', stack, (State.drop_scope state state', stdin, stdout)) ps
+            )
+            | _ -> conf
+    )
+    | insn :: ps -> eval env (eval_insn conf insn) ps
+    | _  -> conf
+
+
+let label =
+  object
+    val mutable counter = 0
+
+    method create =
+        counter <- counter + 1;
+        "l_" ^ string_of_int counter
+  end
 
 (* Top-level evaluation
 
@@ -56,4 +125,90 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile _ = failwith "Not Implemented Yet"
+let rec compile_block stmt =
+    let rec compile_expr expr = match expr with
+        | Expr.Const (n)        -> 
+              [CONST n]
+
+
+        | Expr.Var (x)          -> 
+              [LD x]
+
+        | Expr.Binop (op, x, y) -> 
+              (compile_expr x) 
+            @ (compile_expr y) 
+            @ [BINOP op]
+    in
+    let rec compile_if stmt label_end = match stmt with
+        | Stmt.Seq (a, b)       -> 
+              (compile_block a) 
+            @ (compile_if b label_end)
+
+        | Stmt.If (expr, a, b)  ->
+            let label_else = label#create in
+              (compile_expr expr)
+            @ [CJMP ("z", label_else)]
+            @ (compile_if a label_end)
+            @ [JMP label_end]
+            @ [LABEL label_else]
+            @ (compile_block b)
+        | _ -> compile_block stmt
+    in
+    match stmt with
+    | Stmt.Read (x)         -> 
+              [READ] 
+            @ [ST x]
+
+    | Stmt.Write (expr)     -> 
+              (compile_expr expr) 
+            @ [WRITE]
+
+    | Stmt.Assign (x, expr) -> 
+              (compile_expr expr) 
+            @ [ST x]
+
+    | Stmt.Seq (a, b)       -> 
+              (compile_block a) 
+            @ (compile_block b)
+
+    | Stmt.Skip             -> []
+
+    | Stmt.While (expr, a)  ->
+            let label_cond = label#create in
+            let label_body = label#create in
+              [JMP label_cond; LABEL label_body]
+            @ (compile_block a)
+            @ [LABEL label_cond]
+            @ (compile_expr expr)
+            @ [CJMP ("nz", label_body)]
+
+    | Stmt.Repeat (a, expr)  ->
+            let label_cond = label#create in
+            let label_body = label#create in
+              [LABEL label_body]
+            @ (compile_block a)
+            @ [LABEL label_cond]
+            @ (compile_expr expr)
+            @ [CJMP ("z", label_body)]
+
+    | Stmt.If (expr, a, b) ->
+            let label_end = label#create in
+              (compile_if stmt label_end) 
+            @ [LABEL label_end]
+
+    | Stmt.Call (f, args) ->
+              List.concat(List.map compile_expr (List.rev args)) 
+            @ [CALL f]
+
+let compile_def (f, (arg_names, local_names, stmt)) =
+      [LABEL f]
+    @ [BEGIN (arg_names, local_names)]
+    @ (compile_block stmt)
+    @ [END]
+
+let compile (defs, stmt) =
+    let main = compile_block stmt in
+    let defs = List.concat (List.map compile_def defs) in
+      main
+    @ [END]
+    @ defs
